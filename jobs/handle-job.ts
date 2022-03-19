459f539c0164237c597b322a7010668ba7a3818c
Oleg Krasnikov <a.insolite@gmail.com>
2022-03-19 13:08:22 +0200

    Try to fix jobs handling for dynamic port

--- jobs/handle-job.ts
+++ jobs/handle-job.ts
@@ -4,10 +4,11 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 const host = process.env.HOST!;
+const port = process.env.PORT! || 80;
 const token = process.env.JOBS_API_TOKEN!;
 
 export default function handleJob(name: string): void {
-  axios.get(`${host}/api/jobs/${name}?token=${token}`)
+  axios.get(`${host}:${port}/api/jobs/${name}?token=${token}`)
     .catch((error) => {
       const hasErrorTextInResponse = axios.isAxiosError(error)
         && error.response
