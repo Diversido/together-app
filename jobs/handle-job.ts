ba857d8030e04d90629da41393f1ec62f8360971
Oleg Krasnikov <a.insolite@gmail.com>
2022-03-19 13:57:15 +0200

    Try to fix jobs handling for dynamic port

--- jobs/handle-job.ts
+++ jobs/handle-job.ts
@@ -8,7 +8,7 @@ const port = process.env.PORT! || 80;
 const token = process.env.JOBS_API_TOKEN!;
 
 export default function handleJob(name: string): void {
-  axios.get(`${host}:${port}/api/jobs/${name}?token=${token}`)
+  axios.get(`http://${host}:${port}/api/jobs/${name}?token=${token}`)
     .catch((error) => {
       const hasErrorTextInResponse = axios.isAxiosError(error)
         && error.response
